93d9077b0b908227b9edfb210b3ba99869df3409
shamim-ice <shamim.ice.iu@gmail.com>
2022-04-26 17:33:52 +0000

    Finding accuracy using Naive Bayes Model

--- DeepNLP.py
+++ DeepNLP.py
@@ -11,12 +11,45 @@ from sklearn.utils.validation import check_memory
 stop = set(stopwords.words('english'))
 from wordcloud import WordCloud, STOPWORDS
 
-from sklearn import preprocessing
+from sklearn import preprocessing, metrics
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+NB = MultinomialNB()
 
 #print(check_memory(["ls", "../input"]).decode("utf8"))
 
 data = pd.read_csv('/content/Natural-Language-Processing/Sheet_2.csv', encoding='latin-1')
-print(data.head())
+#print(data.head())
+
+
+def cloud(text):
+  wordcloud  = WordCloud(background_color='red', stopwords = stop).generate(' '.join([i for i in text.str.upper()]))
+  plt.imshow(wordcloud)
+  plt.axis('off')
+  plt.title('Resume Response')
+
+cloud(data['resume_text'])
+
+
+Encode = preprocessing.LabelEncoder()
+
+data['label'] = Encode.fit_transform(data['class'])
+#print(data.head)
+
+x = data['resume_text']
+y = data['label']
+vect = CountVectorizer()
+x = vect.fit_transform(x)
+x_train, x_test, y_train, y_test = train_test_split(x,y,
+random_state=25)
+
+
+
+NB.fit(x_train, y_train)
+y_predict = NB.predict(x_test)
+acc = metrics.accuracy_score(y_test, y_predict)
+print(acc)
+
+
+
